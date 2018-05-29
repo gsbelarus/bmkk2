0848949557bd41390cf3ad7eb582109e543001df
andreik <gs1994@gmail.com>
2018-05-29 14:04:39 +0300

    routes for good groups

--- src/components/Goods.tsx
+++ src/components/Goods.tsx
@@ -4,14 +4,7 @@ import { LoadGoods } from '../actions';
 import { IGoods } from '../types';
 import { goodsFile, goodsRoot } from '../const';
 
-export interface GoodsProps extends PageProps {
-  groupID?: string;
-}; 
-
-export class Goods extends Page<GoodsProps> {
-  constructor(props: GoodsProps) {
-    super(props);
-  }
+export class Goods extends Page {
 
   componentDidMount() {
     const { onLoadGoods } = this.props;
@@ -25,29 +18,29 @@ export class Goods extends Page<GoodsProps> {
   }
 
   renderBody(): JSX.Element {
-    const { goods, selectedLang, groupID  } = this.props; 
-    
+    const { goods, selectedLang, match  } = this.props;
+
+    console.log(this.props);
 
     if (goods) {
       return (
         <div className="GroupsContainer">
-          <div>{groupID}</div>
           {
             goods.goods.map( (g, idx) => (
-              <div className="GoodGroup">
+              <div key={idx}  className="GoodGroup">
                 <img src={`${goodsRoot}${g.image}`} />
                 <div className="GoodGroupName">
                   {g.caption[selectedLang.toLowerCase()].name}
                 </div>
                 <div className="Consist">
                   {g.consist[selectedLang.toLowerCase()].name}
-                </div>     
+                </div>
                 <div className="Casing">
                   {g.casing[selectedLang.toLowerCase()].name}
-                </div>  
+                </div>
                 <div className="Casing">
                   {g.beforuse[selectedLang.toLowerCase()].name}
-                </div>                                           
+                </div>
               </div>
             ))
           }
@@ -56,7 +49,7 @@ export class Goods extends Page<GoodsProps> {
     } else {
       return (
         <div>
-          <div>{groupID}</div>
+          <div>{match.params.groupID}</div>
           Loading...
         </div>
       );
--- src/const.ts
+++ src/const.ts
@@ -131,5 +131,5 @@ export const goodItem: GoodItems = [
         }
       }
     }
-  }     
-]  
+  }
+];
