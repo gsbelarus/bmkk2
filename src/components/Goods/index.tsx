f4853cd2d5ce069baee8eaf9f22570b5eb62d868
andreik <gs1994@gmail.com>
2018-06-11 13:45:32 +0300

    wip

--- src/components/Goods/index.tsx
+++ src/components/Goods/index.tsx
@@ -4,6 +4,17 @@ import { IGoods } from '../../types';
 import { goodsFile, goodsRoot, goodCaption, goodFileNoImage, moreCaption } from '../../const';
 import { WrapText } from '../WrapText/index';
 import { Link } from 'react-router-dom';
+import './goods.css';
+
+/*
+
+          <div className="PriceBtn">
+            {<Link  to={`/price`}>
+              Прейскурант
+            </Link>}
+          </div>
+
+*/
 
 export class Goods extends Page {
 
@@ -23,15 +34,18 @@ export class Goods extends Page {
     console.log(this.props);
 
     if (goods) {
+      const shortenStr = (s: string, maxLength = 80): string => {
+        if (s.length < maxLength) {
+          return s;
+        } else {
+          return `${s.slice(0, maxLength - 3)}...`;
+        }
+      };
+
       const filtered = goods.goods.filter((t) => t.group === match.params.groupID );
 
       return (
         <div>
-          <div className="PriceBtn">
-            {<Link  to={`/price`}>
-              Прейскурант
-            </Link>}
-          </div>
           <div className="GoodsContainer">
             {
               filtered.map( (g, idx) => {
@@ -40,38 +54,37 @@ export class Goods extends Page {
                   : `${goodsRoot}${g.image}`;
 
                 return (
-                  <div key={idx}  className="GoodItem">
-                    <img src={fullImageName} />
-                    <h5 className="GoodName">
-                      {g.caption[selectedLang.toLowerCase()].name}
-                    </h5>
-                    <ul>
-                      <li>
-                        {g.grade[selectedLang.toLowerCase()].name ? g.grade[selectedLang.toLowerCase()].name : '-' }
-                      </li>
-                      <li>
-                        <strong>
-                          {goodCaption.description.ingredients[selectedLang.toLowerCase()].name}
-                        </strong>
-                        <WrapText text={g.ingredients[selectedLang.toLowerCase()].name} />
-                      </li>
-                      <li>
-                        <strong>
-                          {goodCaption.description.casing[selectedLang.toLowerCase()].name}
-                        </strong>
-                        {g.casing[selectedLang.toLowerCase()].name}
-                      </li>
-                      <li>
-                        <strong>
-                          {goodCaption.description.beforuse[selectedLang.toLowerCase()].name}
-                        </strong>
-                        {g.beforuse}
-                      </li>
-                      <Link className="GoodMore" to={`/production/groups/` + g.group + '/good/' + g.ruid}>
-                        {moreCaption.caption[selectedLang.toLowerCase()].name}
-                      </Link>
-                    </ul>
-                  </div>
+                  <Link key={idx} className="GoodMore" to={`/production/groups/` + g.group + '/good/' + g.ruid}>
+                    <div className="GoodItem">
+                      <img src={fullImageName} />
+                      <div className="GoodData">
+                        <div className="GoodName">
+                          {g.caption[selectedLang.toLowerCase()].name}
+                        </div>
+                        <div>
+                          {g.grade[selectedLang.toLowerCase()].name ? g.grade[selectedLang.toLowerCase()].name : 'Без сорта' }
+                        </div>
+                        <div>
+                          <strong>
+                            {`${goodCaption.description.beforuse[selectedLang.toLowerCase()].name} `}
+                          </strong>
+                          {g.beforuse}
+                        </div>
+                        <div>
+                          <strong>
+                            {`${goodCaption.description.casing[selectedLang.toLowerCase()].name} `}
+                          </strong>
+                          {g.casing[selectedLang.toLowerCase()].name}
+                        </div>
+                        <div>
+                          <strong>
+                            {`${goodCaption.description.ingredients[selectedLang.toLowerCase()].name} `}
+                          </strong>
+                          {shortenStr(g.ingredients[selectedLang.toLowerCase()].name)}
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
                 );
               })
             }
