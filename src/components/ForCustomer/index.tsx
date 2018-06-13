616500e06f4934b0595fba55e19954d89386225e
andreik <gs1994@gmail.com>
2018-06-13 16:48:51 +0300

    wip

--- src/components/ForCustomer/index.tsx
+++ src/components/ForCustomer/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Page } from '../Page';
 import * as ReactMarkdown from 'react-markdown';
+import './forcustomer.css';
 
 const md: { [lang: string]: string } = {
   ru: require(`../../../public/data/forcustomer/forcustomer.ru.md`),
@@ -8,9 +9,18 @@ const md: { [lang: string]: string } = {
   en: require(`../../../public/data/forcustomer/forcustomer.en.md`)
 };
 
+const map = require('../../../public/image/map.jpg');
+
 export class ForCustomer extends Page {
   renderBody(): JSX.Element {
     const { selectedLang } = this.props;
-    return (<ReactMarkdown source={md[selectedLang.toLowerCase()]} />);
+    return (
+      <div>
+        <ReactMarkdown source={md[selectedLang.toLowerCase()]} />
+        <a href="https://www.google.com/maps/place/Berozovskiy+Myasokonservnyy+Kombinat/@52.543019,24.9606592,14.5z/data=!4m5!3m4!1s0x4720b6da3c7872d3:0xbf3aaf50876462b4!8m2!3d52.542146!4d24.9549723">
+          <img src={map} />
+        </a>
+      </div>
+    );
   }
 }
