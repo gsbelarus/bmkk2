88f59b5baf875442f312377c79595e74adae4361
andreik <gs1994@gmail.com>
2018-06-20 14:25:45 +0300

    wip

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Page } from '../Page';
 import { IContacts, IDepartments } from '../../types';
-import { contactsFile, departmentsFile, contactCaption } from '../../const';
+import { contactsFile, departmentsFile } from '../../const';
 import './contacts.css';
+import * as ReactMarkdown from 'react-markdown';
+
+const md: { [lang: string]: string } = {
+  ru: require(`../../../public/data/contacts/req.ru.md`),
+  be: require(`../../../public/data/contacts/req.be.md`),
+  en: require(`../../../public/data/contacts/req.en.md`)
+};
 
 export class Contacts extends Page {
 
@@ -23,8 +30,7 @@ export class Contacts extends Page {
   }
 
   renderBody(): JSX.Element {
-    const { contacts, departments, selectedLang } = this.props;
-    const sl = selectedLang.toLowerCase();
+    const { contacts, departments, sl } = this.props;
 
     if (contacts && departments) {
       const depts = departments.departments.filter(
@@ -32,28 +38,31 @@ export class Contacts extends Page {
       );
 
       return (
-        <div className="ContactsContainer">
-          { depts.map( (d, d_idx) => (
-              <div key={d_idx} className="DepartmentItem">
-                <h4>
-                  {d.caption[sl].name}
-                </h4>
-                {
-                  contacts.contacts.filter( c => c.department === d.ruid ).map( (c, idx) => (
-                    <div key={idx} className="ContactItem">
-                      {c.caption[sl].name && <div>{c.caption[sl].name}</div>}
-                      {c.phone && <div><i className="fas fa-phone fa-xs" />{c.phone}</div>}
-                      {c.fax && <div><i className="fas fa-fax fa-xs" />{c.fax}</div>}
-                      {c.email && <div><i className="far fa-envelope fa-xs" />{c.email}</div>}
-                      {c.description && <div>{c.description}</div>}
-                    </div>
+        <div>
+          <ReactMarkdown source={md[sl]} />
+          <div className="ContactsContainer">
+            { depts.map( (d, d_idx) => (
+                <div key={d_idx} className="DepartmentItem">
+                  <h4>
+                    {d.caption[sl].name}
+                  </h4>
+                  {
+                    contacts.contacts.filter( c => c.department === d.ruid ).map( (c, idx) => (
+                      <div key={idx} className="ContactItem">
+                        {c.caption[sl].name && <div>{c.caption[sl].name}</div>}
+                        {c.phone && <div><i className="fas fa-phone fa-xs" />{c.phone}</div>}
+                        {c.fax && <div><i className="fas fa-fax fa-xs" />{c.fax}</div>}
+                        {c.email && <div><i className="far fa-envelope fa-xs" />{c.email}</div>}
+                        {c.description && <div>{c.description}</div>}
+                      </div>
+                      )
                     )
-                  )
-                }
-              </div>
+                  }
+                </div>
+                )
               )
-            )
-          }
+            }
+          </div>
         </div>
       );
     } else {
