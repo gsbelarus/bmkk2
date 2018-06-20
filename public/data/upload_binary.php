0bfecd16dca3f2cca91b603f1e0055d3858f3695
andreik <gs1994@gmail.com>
2018-06-20 18:11:21 +0300

    prevent malicious behaviour

--- public/data/upload_binary.php
+++ public/data/upload_binary.php
@@ -1,21 +1,28 @@
 <?
- 
-function decode($s){ 
-	$out = ""; 
+
+function decode($s){
+	$out = "";
 	$c = 0;
-	while ($c < strlen($s) - 1) 
-	{ 
+	while ($c < strlen($s) - 1)
+	{
     $out = $out.chr(base_convert($s[$c].$s[$c + 1], 16, 10));
 	  $c = $c + 2;
-	} 
-	return $out; 
+	}
+	return $out;
 }
 
 $fn = $_POST['FN'];
 $path = $_POST['PATH'];
 $data = $_POST['DATA'];
-$filename = $path.$fn;
-$file = fopen($filename, "w+"); 
-fwrite($file, decode($data));
+
+$ext = strtolower(substr($fn, -3));
+
+if (strpos($path, '..') === FALSE && substr($path, 0, 1) !== '/') {
+	if ($ext === 'jpg' || $ext === 'json' || $ext === 'png') {
+		$filename = $path.$fn;
+		$file = fopen($filename, "w+");
+		fwrite($file, decode($data));
+	}
+}
 
 ?>
