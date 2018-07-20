b0ea5bbc6a5bcdaaae32e41ffc8f7fd3a3b39ad0
andreik <gs1994@gmail.com>
2018-07-20 14:25:38 +0300

    put images into separate folder

--- webpack.common.js
+++ webpack.common.js
@@ -79,7 +79,7 @@ module.exports =  {
             loader: 'url-loader',
             options: {
                 limit: 8000, // Convert images < 8kb to base64 strings
-                name: '[name].[ext]'//'images/[hash]-[name].[ext]'
+                name: 'images/[hash]-[name].[ext]'
             }
           }
         ]
