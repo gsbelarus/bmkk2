57f5523bda16a0e20be1842ba3769f415cd851b8
sunnycreature <sunnycreature@gmail.com>
2019-07-08 09:40:19 +0300

    upgrade versions

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -8,7 +8,7 @@ module.exports =  {
     app: './src/index.tsx'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Production',
       template: './public/index.html',
